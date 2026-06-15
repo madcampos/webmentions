12d8fa6e81946f9fac41754b968099450f0f6785
madcampos <609904+madcampos@users.noreply.github.com>
2026-06-14 23:28:54 -0400

    feat: move to utils and add error response

---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+export const STATUS_CODES = {
+	OKAY: 200,
+	CREATED: 201,
+	ACCEPTED: 202,
+	BAD_REQUEST: 400,
+	UNAUTHORIZED: 401,
+	FORBIDDEN: 403,
+	NOT_FOUND: 404,
+	INTERNAL_SERVER_ERROR: 500
+} as const;
+
+export type StatusCodes = typeof STATUS_CODES[keyof typeof STATUS_CODES];
+
+export class ErrorResponse extends Response {
+	constructor(message: string, status: StatusCodes = STATUS_CODES.BAD_REQUEST) {
+		super(message, {
+			status,
+			headers: { 'Content-Type': 'text/plain' }
+		});
+	}
+}
