34a4e85618a7942ad380b4942c693451967c835d
madcampos <609904+madcampos@users.noreply.github.com>
2026-06-15 00:52:21 -0400

    feat: add logic to handle webmentions

--- src/index.ts
+++ src/index.ts
@@ -1,155 +1,102 @@
-import { type Context, Hono } from 'hono';
-import type { BlankEnv, BlankInput } from 'hono/types';
-import { parse as parseHtml } from 'node-html-parser';
-import { STATUS_CODES } from './statusCodes.js';
+// oxlint-disable typescript/only-throw-error
 
-const app = new Hono();
+import { checkHTMLIncludesDest, checkJSONIncludesDest, checkTextIncludesDest } from './checker.js';
+import { deleteMention, hasExistingMention, saveWebmention, updateWebmention } from './db.js';
+import { Router } from './router.js';
+import { ErrorResponse, STATUS_CODES, TextResponse } from './utils.js';
+import { parseWebmentionPatameters } from './validation.js';
 
-// TODO: Move to external resources/db
-// TODO: Generate manifest from astro
-// TODO: Save blocklist in KV storage
-// TODO: save list of webmentions in D1 SQLite storage. Use Knex?
-const TARGET_URLS: string[] = ['/blog/2024/09/overly-overlays/'];
-const BLOCKLIST_DOMAINS: string[] = [];
-
-// TODO: move to env
-const BASE_URL = new URL('https://madcampos.dev/');
-
-const ALLOWED_PROTOCOLS = ['http:', 'https:'];
+// oxlint-disable-next-line no-magic-numbers
 const PROCESS_TIMEOUT = 1000 * 60 * 2;
 
-async function parseParameters(context: Context<BlankEnv, '/', BlankInput>) {
-	const contentType = context.req.header('Content-Type');
+const router = new Router();
 
-	if (contentType !== 'application/x-www-form-urlencoded' && !contentType?.startsWith('multipart/form-data')) {
-		throw new Error('Wrong content type');
-	}
+router.get('/', () => new TextResponse('it works!'));
 
-	const data = await context.req.formData();
-	const source = data.get('source');
-	const target = data.get('target');
-
-	if (typeof source !== 'string' || !URL.canParse(source)) {
-		throw new Error('Invalid "source"');
-	}
-
-	const parsedSource = new URL(source);
-
-	if (typeof target !== 'string' || !URL.canParse(target)) {
-		throw new Error('Invalid "target"');
-	}
-
-	if (!ALLOWED_PROTOCOLS.includes(parsedSource.protocol)) {
-		throw new Error('Invalid "source" protocol');
-	}
-
-	if (BLOCKLIST_DOMAINS.includes(parsedSource.hostname)) {
-		throw new Error('GTFO');
-	}
-
-	const parsedTarget = new URL(target);
-
-	if (!ALLOWED_PROTOCOLS.includes(parsedTarget.protocol)) {
-		throw new Error('Invalid "target" protocol');
-	}
-
-	if (BASE_URL.host !== parsedTarget.host) {
-		throw new Error('Your princess is in another castle');
-	}
-
-	if (parsedSource.href === parsedTarget.href) {
-		throw new Error('Paradox not accepted');
-	}
-
-	if (!TARGET_URLS.includes(parsedTarget.pathname)) {
-		throw new Error("I Don't Think It Means What You Think It Means");
-	}
-
-	return {
-		source: parsedSource,
-		target: parsedTarget
-	};
-}
-
-async function validateMention(signal: AbortSignal, source: URL, target: URL) {
-	const response = await fetch(source, { signal });
-
-	if (!response.ok) {
-		throw new Error('Unable to fetch source');
-	}
-
+router.post('/', async (request) => {
 	try {
-		const responseText = await response.text();
-		const root = parseHtml(responseText);
+		const { source, target } = await parseWebmentionPatameters(request);
 
-		// TODO: parse and validate extra data/fields
-
-		const allLinks = root.querySelectorAll('a, link');
-		const urlsForLinks = allLinks
-			.map((link) => URL.parse(link.getAttribute('href') ?? '', source.toString()))
-			.filter((link) => link !== null);
-		const matchingUrls = urlsForLinks.filter((link) => link.hostname === target.hostname && link.pathname === target.pathname);
-
-		return matchingUrls;
-	} catch (err) {
-		console.error(err);
-
-		throw new Error('Unable to process source links');
-	}
-}
-
-async function processMention(source: URL, target: URL) {
-	try {
 		const timeoutController = new AbortController();
 
 		setTimeout(() => timeoutController.abort(), PROCESS_TIMEOUT);
 
-		const linksWithTarget = await validateMention(timeoutController.signal, source, target);
+		const response = await fetch(source, { signal: timeoutController.signal });
 
-		if (linksWithTarget.length < 1) {
-			throw new Error("Source doesn't mention target");
+		if (!response.ok && response.status !== STATUS_CODES.GONE) {
+			throw new ErrorResponse('Unable to fetch source');
 		}
 
-		// TODO: check if it is an update
+		const hasMention = await hasExistingMention(source.href, target.href);
 
-		// TODO: save to db
+		if (response.status === STATUS_CODES.GONE) {
+			if (!hasMention) {
+				throw new ErrorResponse('Web mention does not exist');
+			}
+
+			await deleteMention(source.href, target.href);
+			return new TextResponse('Webmention deleted');
+		}
+
+		const contentType = response.headers.get('Content-Type');
+
+		let sourceHasTarget = false;
+
+		if (contentType?.includes('text/html')) {
+			sourceHasTarget = await checkHTMLIncludesDest(response, source, target);
+		} else if (contentType?.includes('application/json')) {
+			sourceHasTarget = await checkJSONIncludesDest(response, target);
+		} else {
+			sourceHasTarget = await checkTextIncludesDest(response, target);
+		}
+
+		if (!sourceHasTarget) {
+			if (!hasMention) {
+				throw new ErrorResponse("Source doesn't mention target");
+			}
+
+			await deleteMention(source.href, target.href);
+			return new TextResponse('Webmention deleted');
+		}
+
+		if (hasMention) {
+			await updateWebmention(source.href, target.href);
+			return new TextResponse('Webmention updated');
+		}
+
+		await saveWebmention(source.href, target.href);
+		return new TextResponse('Webmention created');
 	} catch (err) {
-		console.error(err);
-	}
-}
+		if (err instanceof ErrorResponse) {
+			return err;
+		}
 
-app.get('/', (context) => {
-	context.status(STATUS_CODES.OKAY);
-
-	return context.text('it works');
-});
-
-app.post('/', async (context) => {
-	try {
-		const { source, target } = await parseParameters(context);
-
-		await processMention(source, target);
-
-		context.status(STATUS_CODES.OKAY);
-		return context.text('Your offer was accepted');
-	} catch (err) {
 		if (err instanceof Error) {
-			context.status(STATUS_CODES.BAD_REQUEST);
-			return context.text(err.message);
+			return new ErrorResponse(err.message);
 		}
 
-		context.status(STATUS_CODES.INTERNAL_SERVER_ERROR);
-
-		return context.text('Oops...');
+		return new ErrorResponse('Oops...', STATUS_CODES.INTERNAL_SERVER_ERROR);
 	}
 });
 
-app.get('/mentions', async (context) => {
-	// TODO: retun mentions for url
-});
+router.get('/mentions', () =>
+	// TODO: return mentions for a url
+	new Response('it works!', {
+		status: STATUS_CODES.OKAY,
+		headers: {
+			'Content-Type': 'text/plain'
+		}
+	}));
 
-app.post('/block', async (context) => {
+router.post('/block', () =>
 	// TODO: create blocklist and ban domains
-});
 
-export default app;
+	new Response('it works!', {
+		status: STATUS_CODES.OKAY,
+		headers: {
+			'Content-Type': 'text/plain'
+		}
+	}));
+
+// oxlint-disable-next-line import/no-default-export
+export default router;
