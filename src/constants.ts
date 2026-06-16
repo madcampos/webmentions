e5412143f04b09581207d114996524a9720719ae
madcampos <609904+madcampos@users.noreply.github.com>
2026-06-15 23:21:28 -0400

    feat: move all constants to a constants file

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+// oxlint-disable no-magic-numbers
+
+import { env } from 'cloudflare:workers';
+
+export const PROCESS_TIMEOUT = 1000 * 60 * 2;
+export const ALLOWED_ORIGINS = env.ALLOWED_ORIGINS.split(';');
+export const ALLOWED_PROTOCOLS = ['http:', 'https:'];
+export const TARGET_PATHS: string[] = env.TARGET_PATHS.split(';');
+export const MAX_CONTENT_LENGTH = 5 * 1024 * 1024;
+export const DEFAULT_WINDOW_SIZE = 5;
+export const DEFAULT_WEBMENTION_TIMEOUT_MS = 1 * 60 * 60 * 1000;
